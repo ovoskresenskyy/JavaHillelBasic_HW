2f10e2d9d54247665585fd54fb5887278783b9c9
Саша <7949894@ukr.net>
2022-09-02 00:29:07 +0300

    Добавлена вторая строка. Интересная штука гит, но надо еще разбираться :)

--- src/HW_19_GitHub/MainApp.java
+++ src/HW_19_GitHub/MainApp.java
@@ -3,5 +3,6 @@ package HW_19_GitHub;
 public class MainApp {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
+        System.out.println("Looks like i start to understand how to use you!");
     }
 }
